033aa737cab91a711429c693fe3a7c6a3ba8cdb3
Renan <renancvr777@gmail.com>
2025-10-21 15:53:25 -0300

    feat: adding rate limiting and test to validate it (10 requests/1 second)

--- apps/api-gateway/src/app.controller.spec.ts
+++ apps/api-gateway/src/app.controller.spec.ts
@@ -1,6 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { INestApplication } from '@nestjs/common';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import * as request from 'supertest';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -17,6 +22,50 @@ describe('AppController', () => {
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+
+  describe('AppController (e2e)', () => {
+    let app: INestApplication;
+
+    beforeAll(async () => {
+      const moduleFixture: TestingModule = await Test.createTestingModule({
+        imports: [
+          ThrottlerModule.forRoot({
+            throttlers: [
+              {
+                ttl: 1000,
+                limit: 10,
+              },
+            ],
+          }),
+        ],
+        controllers: [AppController],
+        providers: [
+          AppService,
+          {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard,
+          },
+        ],
+      }).compile();
+
+      app = moduleFixture.createNestApplication();
+      await app.init();
+    });
+
+    it('should throttle requests', async () => {
+      const url = '/';
+
+      for (let i = 0; i < 10; i++) {
+        await request(app.getHttpServer()).get(url).expect(200);
+      }
+
+      await request(app.getHttpServer()).get(url).expect(429);
+    });
+
+    afterAll(async () => {
+      await app.close();
     });
   });
 });
--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 1000,
+          limit: 10,
+        },
+      ],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
