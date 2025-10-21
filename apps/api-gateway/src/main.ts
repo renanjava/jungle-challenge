b16341c8ccfd3d03e3261d34489502f58ea08e90
Renan <renancvr777@gmail.com>
2025-10-21 16:09:59 -0300

    feat: adding enable cors and helmet to security in header requests

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Jungle Challenge')
