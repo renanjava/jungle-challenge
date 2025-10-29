1067c5f3c57942a9cdce8f7ca9e799582218bfe0
renanjava <renancvr777@gmail.com>
2025-10-29 00:13:16 -0300

    build: generate first component with shadcn/ui

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,7 +1,15 @@
+import {Button} from './components/ui/button'
+
 export default function App() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
       <h1 className="text-4xl font-bold">Hello Tailwind! 🌟</h1>
+       <div className="p-4 space-x-2">
+      <Button>Default</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="destructive">Destructive</Button>
+    </div>
     </div>
   );
 }
